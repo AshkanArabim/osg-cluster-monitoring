88d71d19b324947903363234ba1eae7ceefb5bbc
AshkanArabim <ashkan.arabim@gmail.com>
2025-05-29 19:18:30 -0600

    add stack deployment command to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ mkdir -p ./storage
 
 # replace env vars in docker-stack.yaml
 envsubst < docker-stack-template.yaml > docker-stack.yaml
+
+# start the stack
+docker stack deploy --detach=false -c docker-stack.yaml ldms
